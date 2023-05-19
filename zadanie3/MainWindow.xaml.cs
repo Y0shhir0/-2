1c096c343898a3758a84a31926f927e6b9dc7888
DenisS <semenovdenis3201@gmail.com>
2023-05-19 21:08:22 +0300

    Добавьте файлы проекта.

---
+++ zadanie3/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadanie3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(Year.Text, out int dayOfYear))
+            {
+                int week = Week(dayOfYear);
+                string weekName = WeekName(week);
+                resultLabel.Content = weekName;
+            }
+            else
+            {
+                resultLabel.Content = "Неверный ввод";
+            }
+        }
+        private int Week(int dayOfYear)
+        {
+            int dayweek = 7; //  2023 1 января
+            int week = (dayweek + dayOfYear - 1) % 7;
+            return week == 0 ? 7 : week;
+        }
+
+        private string WeekName(int week)
+        {
+            string[] dayOfWeekNames = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+            return dayOfWeekNames[week - 1];
+        }
+    }
+}
