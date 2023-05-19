ba9340b06ae7ea33a08440a374ce3926e73799eb
DenisS <semenovdenis3201@gmail.com>
2023-05-19 21:19:03 +0300

    Добавление 1 задания

---
+++ zadanie1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadanie1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            int number;
+            if (int.TryParse(Text.Text, out number))
+            {
+                int s1 = number / 100; // Получение первой цифры числа (сотни)
+
+                Text.Text = $"Первая цифра: {s1}";
+            }
+            else
+            {
+                Text.Text = "Некорректное число!";
+            }
+        }
+    }
+}
