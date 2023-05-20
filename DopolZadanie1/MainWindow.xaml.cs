809a8125557803807c9853e03aad28a8c5cabd85
tmbh <artem555game1@gmail.com>
2023-05-20 11:33:44 +0300

    Добавил 1 допол.задание

---
+++ DopolZadanie1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DopolZadanie1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int A = Convert.ToInt32(TbNumberA.Text);
+                int B = Convert.ToInt32(TbNumberB.Text);
+                int C = Convert.ToInt32(TbNumberC.Text);
+                int k = (A / C) * (B / C);
+                int a = (A * B) - (k * C * C);
+                TextBlockAnswer.Text = $"Ответ:Количество квадратов = {k:f2}" + $"Площадь незанятой части прямоугольника = {a:f2}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }          
+        }
+    }
+}
